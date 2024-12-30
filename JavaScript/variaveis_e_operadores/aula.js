a7daa7f37943a559475ef49d6143f90c58e2cbd1
JonatasPablo <jonathaspabllo@gmail.com>
2024-12-30 15:09:40 -0300

    Aulas

---
+++ JavaScript/variaveis_e_operadores/aula.js
@@ -0,0 +1,10 @@
+// formas de declarar variáveis
+// var, let e const
+let variavel = 10;
+// por ser constante, não é possível alterar o valor
+const pi = 3.14;
+// por ser variável, é possível alterar o valor
+variavel = 20;
+
+variavel = variavel / 10;
+console.log(variavel);
---
+++ Lógica de Programação/src/script.js
@@ -0,0 +1,5 @@
+const message = 'Hello World!'
+
+document.querySelector('#header').innerHTML = message
+
+console.log(message)
